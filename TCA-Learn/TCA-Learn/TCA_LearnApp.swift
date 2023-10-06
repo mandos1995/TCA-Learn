f6b05693ffc29afce812b26d70e3bda53447318d
mandos1995 <mandos19950620@gmail.com>
2023-10-06 15:20:29 +0900

    chore: 프로젝트 생성

---
+++ TCA-Learn/TCA-Learn/TCA_LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_LearnApp.swift
+//  TCA-Learn
+//
+//  Created by 김민석 on 2023/10/06.
+//
+
+import SwiftUI
+
+@main
+struct TCA_LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
